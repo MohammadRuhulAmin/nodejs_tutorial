8ebe1e2a9c6446d28b3c839842db12bcac60f5d7
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2021-02-15 01:01:10 +0600

    EventEmmitter

--- app.js
+++ app.js
@@ -1,8 +1,45 @@
-// const tutorial = require('./tutorial');
+const EventEmitter = require('events');
 
-// console.log(tutorial(1,4));
+const myEvent = require('events');
+const eventofMy = new myEvent();
 
-const tutorial = require('./tutorial');
-console.log(tutorial.divition(1,22));
-console.log(tutorial.substraction(233,44));
+const eventEmitter = new EventEmitter();
 
+eventEmitter.on('tutorial',()=>{
+    console.log("tutorial event has occured!");
+
+});
+
+eventEmitter.emit('tutoria');
+
+eventofMy.on('xhttp',function(){
+    console.log("ok i am fired!");
+});
+
+eventofMy.emit('xhttp');
+
+const eventX = require('events');
+const summation = new eventX();
+
+ summation.on('sum',(num1,num2)=>{
+   console.log(num1+num2);
+});
+
+summation.emit('sum',12,222);
+
+class Person extends EventEmitter{
+    constructor(name){
+        super();
+        this._name = name;
+    }
+    get name(){
+        return this._name;
+    }
+}
+
+
+let pedro = new Person('pedro');
+pedro.on('name',function(){
+    console.log('name is : ' + pedro.name);
+});
+pedro.emit('name');
