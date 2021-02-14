f664f181ec46d7947d83b8fc9eb8015e50709c39
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2021-02-15 00:43:31 +0600

    import & export

---
+++ app.js
@@ -0,0 +1,8 @@
+// const tutorial = require('./tutorial');
+
+// console.log(tutorial(1,4));
+
+const tutorial = require('./tutorial');
+console.log(tutorial.divition(1,22));
+console.log(tutorial.substraction(233,44));
+
---
+++ tutorial.js
@@ -0,0 +1,25 @@
+
+const multiplication = function(num1,num2){
+    return num1*num2;
+}
+
+const divition = (num1,num2)=>num1/num2;
+const substraction = (num1,num2)=>num1-num2;
+const addition = (num1,num2)=>num1+num2;
+
+// module.exports.sum = sum;
+// module.exports.PI = PI;
+// module.exports.someMathObject = someMathObject;
+
+// module.exports = { 
+//     sum:sum,
+//     PI:PI,
+//     someMathObject:someMathObject
+
+// }
+
+module.exports.multiplication = multiplication;
+module.exports.divition = divition;
+module.exports.substraction = substraction;
+
+
