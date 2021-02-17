1df97f7376423cd74cef783619f5a07eadaf5c78
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2021-02-17 07:03:39 +0600

    http request response is added

---
+++ httpModule.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const server = http.createServer((request,response)=>{
+    if(request.url === '/'){
+        request.write('khi everyone! its from node js'):
+        request.end();
+    }
+    else{
+        response.write('use another domain');
+        response.end();
+    }
+    // response.write('hellow world from modejs');
+    // response.end();
+});
+
+server.listen('3000');
